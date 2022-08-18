d720a46feb243fb1c73e4707e64322ae237103e9
Zanalis <108939045+Zanalis@users.noreply.github.com>
2022-08-18 00:10:00 -0300

    List

--- meu-projeto/src/components/Item.js
+++ meu-projeto/src/components/Item.js
@@ -1,7 +1,17 @@
-function Item(props){
+/*function Item(props){ utilizando props
     return(
         <>
         <li>{props.marca}</li>
+        </>
+    )
+}
+
+export default Item*/
+
+function Item({marca, ano_lancamento}){  //passando o objeto
+    return(
+        <>
+        <li>{marca} - {ano_lancamento}</li>
         </>
     )
 }
