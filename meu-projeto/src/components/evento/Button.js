f3deae284d4cb7e783769342f1cb200160b4c497
Zanalis <108939045+analyzze@users.noreply.github.com>
2022-09-13 13:28:27 -0300

    Lis

--- meu-projeto/src/components/evento/Button.js
+++
@@ -1,6 +0,0 @@
-//props enviar funções atraves de componentes filhos para componentes pais
-function Button(props){
-
-    return <button onClick={props.event}>{props.text}</button>
-}
-export default Button
