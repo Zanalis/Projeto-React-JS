b430a1ed3e24c1426a2ea4aff30921d7f751d473
Zanalis <108939045+analyzze@users.noreply.github.com>
2022-09-14 18:37:38 -0300

    Criação das rotas

--- sistema/src/App.js
+++ sistema/src/App.js
@@ -1,24 +1,27 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Empresa from './components/pages/Empresa';
+import Contato from './components/pages/Contato';
+import NovoProjeto from './components/pages/NovoProjeto';
 
 function App() {
   return (
-   <Router>
-    <ul>
-      <li>Home</li>
-      <li>Empresa</li>
-      <li>Contato</li>
-      <li>Novo Projeto</li>  
-    </ul>
-    <Switch>
-      <Route exact path="/"> <Home /> </Route>
-      <Route exact path="/empresa"> <Empresa /> </Route>
-      <Route exact path="/contato"> <Contato /> </Route>
-      <Route exact path="/novoprojeto"> <NovoProjeto /> </Route>
-
-    </Switch>
-    <p>Footer</p>
-   </Router>
-  );
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/empresa">Empresa</Link>
+        <Link to="/contato">Contato</Link>
+        <Link to="/novoprojeto">Novo Projeto</Link>
+      </div>
+      <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route exact path="/empresa"><Empresa/></Route>
+        <Route exact path="/contato"><Contato /></Route>
+        <Route exact path="/novoprojeto"><NovoProjeto /></Route>
+      </Switch>
+    </Router>
+    
+  )
 }
 
 export default App;
